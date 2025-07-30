24209a64497253296b9d555dce41d0da09180897
ladyjean <ladyjeanpepito1992@gmail.com>
2025-07-30 22:58:23 +0800

    message

---
+++ src/pepito/ACT3.java
@@ -0,0 +1,23 @@
+
+package pepito;
+
+import java.util.Scanner;
+
+public class ACT3 {
+    
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (num == 0) {
+            System.out.println("Inputted number Zero");
+        } else if (num > 0) {
+            System.out.println(num + " is a positive number");
+        } else {
+            System.out.println(num + " is a negative number");
+        }
+    }
+}
+
